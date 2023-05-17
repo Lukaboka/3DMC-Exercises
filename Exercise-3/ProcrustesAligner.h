9b28ac62a3978d4a3a743d49ea11cfe15d1472b3
Lukas Liu <lukasliu@web.de>
2023-05-17 17:43:35 +0200

    updated rotation matrix function

--- Exercise-3/ProcrustesAligner.h
+++ Exercise-3/ProcrustesAligner.h
@@ -21,6 +21,12 @@ public:
 
 		Matrix4f estimatedPose = Matrix4f::Identity();
 
+		/*
+		* r_x 0   0   t_x
+		* 0   r_y 0   t_y
+		* 0   0   r_z t_z
+		* 0   0   0   1
+		*/
 		estimatedPose.block(0, 0, 3, 3) = rotation;
 		estimatedPose.block(0, 3, 3, 1) = translation;
 
@@ -46,8 +52,39 @@ private:
 		// To compute the singular value decomposition you can use JacobiSVD() from Eigen.
 		// Hint: You can initialize an Eigen matrix with "MatrixXf m(num_rows,num_cols);" and access/modify parts of it using the .block() method (see above).
 
-		// X = target object point cloud, X^ = moved object point cloud
-		Matrix3f rotation = Matrix3f::Identity(); 
+		// X = target object point cloud, X_hat = moved object point cloud
+		MatrixXf X = MatrixXf(3, sourcePoints.size());
+		MatrixXf X_hat = MatrixXf(3, targetPoints.size());
+
+		// create matrix X and X_hat from point vectors
+		for (int i = 0; i < sourcePoints.size(); i++)
+		{
+			X.block(0, i, 3, 1) = sourcePoints[i] - sourceMean;
+			X_hat.block(0, i, 3, 1) = targetPoints[i] - targetMean;
+		}
+
+		Matrix3f A = X * X_hat.transpose();
+
+		// SVD of X * X_hat
+		MatrixXf U = A.jacobiSvd(ComputeFullU);
+		MatrixXf V = A.jacobiSvd(ComputeFullV);
+
+		Matrix3f rotation;
+
+		// if det(U*V^T) == -1 mirror the rotation matrix 
+		if ((U * V.transpose()).determinant() == -1)
+		{
+			Matrix3f m;
+			m << 1, 0, 0,
+				0, 1, 0,
+				0, 0, -1;
+			rotation = U * m * V.transpose();
+		}
+		else
+		{
+			rotation = U * V.transpose();
+		}
+
         return rotation;
 	}
 
