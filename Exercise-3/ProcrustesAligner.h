8e91aa5e17e705ad64eafc4572deb6571e3b79c1
Lukas Liu <lukasliu@web.de>
2023-05-18 15:52:09 +0200

    copium

--- Exercise-3/ProcrustesAligner.h
+++ Exercise-3/ProcrustesAligner.h
@@ -22,9 +22,9 @@ public:
 		Matrix4f estimatedPose = Matrix4f::Identity();
 
 		/*
-		* r_x 0   0   t_x
-		* 0   r_y 0   t_y
-		* 0   0   r_z t_z
+		* rxx rxy rxz t_x
+		* ryx ryy ryz t_y
+		* rzx rzy rzz t_z
 		* 0   0   0   1
 		*/
 		estimatedPose.block(0, 0, 3, 3) = rotation;
@@ -53,23 +53,26 @@ private:
 		// Hint: You can initialize an Eigen matrix with "MatrixXf m(num_rows,num_cols);" and access/modify parts of it using the .block() method (see above).
 
 		// X = target object point cloud, X_hat = moved object point cloud
-		MatrixXf X = MatrixXf(3, sourcePoints.size());
-		MatrixXf X_hat = MatrixXf(3, targetPoints.size());
+		MatrixXf X = MatrixXf(sourcePoints.size(), 3);
+		MatrixXf X_hat = MatrixXf(sourcePoints.size(), 3);
 
-		// create matrix X and X_hat from point vectors
+		// create matrix X and X_hat from point vectors as rows and center them
 		for (int i = 0; i < sourcePoints.size(); i++)
 		{
-			X.block(0, i, 3, 1) = sourcePoints[i] - sourceMean;
-			X_hat.block(0, i, 3, 1) = targetPoints[i] - targetMean;
+
+			X.block(i, 0, 1, 3) = sourcePoints[i].transpose() - sourceMean.transpose();
+			X_hat.block(i, 0, 1, 3) = targetPoints[i].transpose() - targetMean.transpose();
 		}
 
-		Matrix3f A = X * X_hat.transpose();
+		Matrix3f A = X.transpose() * X_hat;
 
 		// SVD of X * X_hat
-		MatrixXf U = A.jacobiSvd(ComputeFullU);
-		MatrixXf V = A.jacobiSvd(ComputeFullV);
+		JacobiSVD<Matrix3f> svd(A, ComputeFullU | ComputeFullV);
 
-		Matrix3f rotation;
+		MatrixXf U = svd.matrixU();
+		MatrixXf V = svd.matrixV();
+
+		Matrix3f rotation = Matrix3f::Zero();
 
 		// if det(U*V^T) == -1 mirror the rotation matrix 
 		if ((U * V.transpose()).determinant() == -1)
@@ -92,6 +95,7 @@ private:
 		// TODO: Compute the translation vector from source to target points.
 
 		Vector3f translation = - rotation * sourceMean + targetMean;
+		
         return translation;
 	}
 };
